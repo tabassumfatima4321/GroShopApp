b84449955d417a8b9e8c47e5c0cd63e20de02032
tabassumfatima4321 <tabassum.fatima4321@gmail.com>
2023-01-29 18:01:24 +0500

    Initial commit

---
+++ app/src/main/java/com/example/groshopapp/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.groshopapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Green=Color(0xFF689C36)
+val LightGreen=Color(0xFFCDF1AA)
+val Yellow=Color(0xFFFFD12E)
+val Red=Color(0xFFDF2E2E)
+val LightGray=Color(0xFFDADDD8)
+val MidGray=Color(0xFFACADAC)
+val GreenishGray=Color(0xFF22281E)
+val White=Color(0xFFF7FFFA)
+val BLACK=Color(0xFFF191A19)
+
